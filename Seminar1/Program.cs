955c56abb56132903827fec1f5917012faf7e9eb
sergey ogurtsov <sergey.ogurtsov@gmail.com>
2024-07-25 17:24:28 +0300

    Добавили ввод числа для

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -61,8 +61,8 @@ int thirdValue = 35;
 */
 
 //Задача 4
-
-int number = 24;
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
         for (int i = 1; i <= number; i++)
         {
             if (i % 2 == 0)
