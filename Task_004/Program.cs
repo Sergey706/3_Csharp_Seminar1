9fb336befa70c37564a913836321ea3ac38e53de
sergey ogurtsov <sergey.ogurtsov@gmail.com>
2024-07-25 08:27:35 +0300

    Vipolnili Task_004 большее из двух чисел

---
+++ Task_004/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        // Введите свое решение ниже
+        if (firstNumber == secondNumber)
+        {
+            System.Console.Write($"Числа равны");
+        }
+        else{
+            if (firstNumber > secondNumber)
+            {
+                System.Console.Write($"Первое число больше");
+            }
+            else System.Console.Write($"Второе число больше");
+        }
+
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 6;
+            secondNumber = 5;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
